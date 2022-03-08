96dae290ba7fb15e8a5679bd56ba535392d55c7f
José Carlos <jc.bsfilho@gmail.com>
2022-03-08 17:22:10 -0300

    test env

--- serverless.ts
+++ serverless.ts
@@ -25,6 +25,7 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       MS_TABLE: '${self:service}-${opt:stage, self:provider.stage}',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      VERSION_APP: '${file(.env):VERSION_APP}',
     },
     lambdaHashingVersion: '20201221',
     iam: {
